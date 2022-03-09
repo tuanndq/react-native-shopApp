e5ef67384a4d3e78ec734caf7ab33d83f56dbcde
Tuanndq <quangtuanxyz@gmail.com>
2022-03-09 16:45:55 +0700

    add categoryListItem

---
+++ components/CategoryListItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Image,
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+import SkiiImage from '../assets/ski.png'
+
+const CategoryListItem = (props) => {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.itemTitle}>CategoryListItem</Text>
+      <Image source={SkiiImage} style={styles.image} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    elevation: 10,
+    alignItems: 'center',
+    padding: 16,
+    borderRadius: 4,
+    backgroundColor: '#fff',
+    marginBottom: 16,
+  },
+  itemTitle: {
+    textTransform: 'uppercase',
+    marginBottom: 8,
+    fontWeight: '700',
+  },
+  image: {
+    width: 64,
+    height: 64,
+  },
+})
+
+export default CategoryListItem;
